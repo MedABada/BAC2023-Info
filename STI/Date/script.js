c1d78b6c1d518389019717f3e2c33c5946fe6e26
MedABada <theazmek@gmail.com>
2022-08-18 16:39:57 +0100

    Date :)

---
+++ STI/Date/script.js
@@ -0,0 +1,88 @@
+mat=document.getElementById("txtMat");
+nom=document.getElementById("txtNom");
+dateL=document.getElementById("dateLoc");
+nbJr=document.getElementById("txt_nbJour");
+function verif() 
+{
+    verif_matricule();
+    verif_nom();
+    verif_date(dateL.value);
+}
+
+function verif_matricule()
+{
+    if (mat.value.length == 12) 
+    {   
+        ch = mat.value;
+        m1 = ch.substr(0,3);
+        m2 = ch.substr(3,5);
+        m3 = ch.substr(-4);
+
+        if (!isNaN(m1) && m2.toUpperCase() == "TUNIS" && !isNaN(m3)) 
+        {
+            return(true);
+        }
+        else
+        {
+            alert("Jawek Moch Behi");
+        }
+    }
+}
+
+function verif_nom() 
+{
+    if (isAlpha(nom.value) == true) 
+    {
+        return(true);
+    }
+    else
+    {
+        alert("Esmk ma3jebnich");
+    }
+}
+
+function isAlpha(ch) 
+{
+    if (ch.length>0) 
+    {
+        i=0;
+        ch = ch.toUpperCase();
+        if(ch[0]==" " || ch[ch.length-1]==" ")
+        {
+            alert("erreur");
+            return false;
+        }
+        while ((ch[i]>="A" && ch[i]<= "Z") || ch[i]==" " && i<ch.length) 
+        {
+            i = i+1;      
+        }    
+    }
+    return i == ch.length;
+}
+
+function verif_date(date) 
+{
+    j = Number(date.substr(0,2));
+    m = Number(date.substr(3,2));
+    a = Number(date.substr(6,4));
+
+    if (!isNaN(j) && !isNaN(m) && !isNaN(a)) 
+        {
+            if ((j<=31 && j>=1) && (m >= 1 && m<=12) && (a>=2022))
+            {
+                dateRetour = new Date (a, m, j);
+                dateRetour.setDate(Number(nbJr.value)+dateRetour.getDate());
+                console.log(dateRetour.getDate());
+           
+            }
+            else
+            {   alert("erreur date");
+                return (false);
+            }
+        }
+    else
+        {   alert("erreur date");
+            return(false);
+        }
+    return
+}
